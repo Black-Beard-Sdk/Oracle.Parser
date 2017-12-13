c314cbad539728844526323ffe4ce68b4def273c
gaelgael <gaelgael5@gmail.com>
2017-12-13 09:58:34 +0100

    fixe on grant parser and manged error

---
+++ Src/Black.Beard.Oracle.Parser.UnitTest/OracleLoaderUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bb.Beard.Oracle.Reader;
+using System.IO;
+
+namespace Black.Beard.Oracle.Parser.UnitTest
+{
+    [TestClass]
+    public class OracleLoaderUnitTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string connectionString = string.Format(@"Data source={0};USER ID={1};Password={2};", "DEV_V", "GUEST", "GUEST");
+            Func<string, bool> act = shema => { return true; };
+            string file = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+
+            Database.GenerateFile("sourceName", connectionString, file, act, false, "Name");
+            
+        }
+
+    }
+}
